6247ef46f71ab194755f7c9c0b6511ddcdc3e94a
Sandeep Reddy <sandeepreddykothakota08@gmail.com>
2024-08-27 20:48:38 +0530

    Created 3 containers

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const app = express();
 connectDb().catch(err => console.log(err));
 app.use(express.json());
 app.use('/api/books',bookRouter);
+app.get('/whoami',(req,res)=>{
+    const result = {
+        message: `${process.env.SERVER_NAME || 'unknown server'}`
+    }
+    res.send(result);
+});
 app.listen(3000,()=>console.log("Listening at localhost:3000"));
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,17 +5,22 @@ const bookRouter = new express.Router();
 
 // Get
 bookRouter.get("/", async (req,res)=>{
-    var {page,limit,sort,query}  = req.query;
+    var page = 1;
+    var limit = 20;
+    var sort = "title";
+    if(req.query.page) page = req.query.page;
+    if(req.query.limit) limit = req.query.limit;
+    if(req.query.sort) sort = req.query.sort;
+    if(limit >= 100) limit = 100;
     var sortingOrder = 1;
 
-    if(sort.charAt(0) === '-'){
+    if(sort && sort.charAt(0) === '-'){
         sortingOrder = -1;
         sort = sort.slice(1,sort.length);
     }
     var sortArg = {};
     sortArg[sort] = sortingOrder;
     const data = await Book.find().limit(limit).skip(page).sort(sortArg);
-    console.log(page , limit, sort,query)
     res.send(data);
 });
 
@@ -44,9 +49,31 @@ bookRouter.post("/", async (req,res)=>{
 // Search for book by filter
 bookRouter.get("/filter", async (req,res)=>{
     const query = req.query;
-    console.log(query);
     const data = await Book.find(query);
     res.send(data);
+});
+
+// Update Book using PUT
+bookRouter.put("/:id",async (req,res)=>{
+    const id = req.params.id;
+    await Book.findByIdAndDelete(id);
+    const newRecord = new Book(req.body);
+    await newRecord.save();
+    res.send(newRecord.toJSON());
+});
+
+// Update Book using PATCH
+bookRouter.patch("/:id",async (req,res)=>{
+    const id = req.params.id;
+    const updateRes = await Book.findByIdAndUpdate(id,req.body);
+    res.send(updateRes);
+});
+
+// Delete Book by id using DELETE
+bookRouter.delete("/:id",async (req,res)=>{
+    const id = req.params.id;
+    const updateRes = await Book.findByIdAndDelete(id);
+    res.send(updateRes);
 });
 
 
